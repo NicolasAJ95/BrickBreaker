1a413e0ea34b25bbed0b80348071c8646b763c01
NicolasAJ95 <nicolas.ajaramillo@live.com>
2016-11-03 01:22:52 -0500

    0.1.0

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public Paddle paddle;
+    private bool hasStarted = false;
+    private bool grounded = true;
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+	
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+        }
+        if (Input.GetMouseButtonDown(0) && grounded)
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 10f);           
+            grounded = false;
+        }
+
+
+
+	}
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    void OnTriggerEnter2D (Collider2D looseCollider)
+    {
+        print("Trigger");
+        levelManager.LoadLevel ("Win");
+    }
+
+
+
+
+}
